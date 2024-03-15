b69dc9450135a0f21069e232751bedc7e41dd247
goranovore2.0 <59402067+goqeq@users.noreply.github.com>
2024-03-15 07:57:53 +0100

    Update menu.ino

--- dev_arduino/design_menu/menu/menu.ino
+++ dev_arduino/design_menu/menu/menu.ino
@@ -6,7 +6,9 @@ TFT_eSPI tft = TFT_eSPI();
 TFT_eSprite background = TFT_eSprite(&tft);
 TFT_eSprite menu = TFT_eSprite(&tft);
 
-int constant = 0;
+bool constant = false, locked = constant;
+
+int y_position_locked = 0;
 
 /*
 0 : NONE
@@ -32,45 +34,42 @@ void setup() {
   background.fillSprite(TFT_RED);
 }
 
-bool locked = false;
-int y_position_locked = 0;
-
 void loop() {
   touch.available();
 
-  if (touch.data.points && (touch.data.y <= 50 || constant == 1 || (touch.data.y <= 50))  && !locked){
+  if (touch.data.points && (touch.data.y <= 50 || constant == 1) && !locked){
+    y_position_locked = touch.data.y;
     background.fillSprite(TFT_RED);
     menu.pushToSprite(&background, 0, -240+touch.data.y);
     constant = 1;
   }
-  
-  else if (constant == 1){ // le doigt quitte l'écran
-      //Serial.println(touch.data.y);
-      if (50 < touch.data.y && touch.data.y <= 130){ // remonter
-      }
-      if (touch.data.y > 130 || touch.data.gestureID == 2){ // descendre
-        Is_locked();
-      }
-      else {
-        background.fillSprite(TFT_RED);
-      }
+
+  else{
+    locked = false;
+
+    if (touch.data.y < 130 && constant == 1){
+      background.fillSprite(TFT_RED); 
+    }
+
+    else if (touch.data.y >= 130 || touch.data.gestureID == 2){
+      Is_Locked();
+    }
+
     constant = 0;
   }
 
-  if (locked){
-    if (y_position_locked > 240){
-      y_position_locked = 240;
-    }
-    Serial.println(y_position_locked);
-    menu.pushToSprite(&background, 0, -240+y_position_locked);
-    y_position_locked += 40; // augmente la rapidité
-  }
-  
   background.pushSprite(0,0);
 }
 
 
-void Is_locked(){
+void Is_Locked(){
   locked = true;
-  y_position_locked = touch.data.y;
+  y_position_locked += 40; // rapidité de défilement
+
+  if (y_position_locked >= 240){
+    y_position_locked = 240;
+  }
+
+  menu.pushToSprite(&background, 0, -240+y_position_locked);
 }
+
